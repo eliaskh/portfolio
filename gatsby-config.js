b79567a80d2aae41927c2450de7750acee771c04
elias <eliaskhs@hotmail.com>
2022-05-08 15:52:50 +0300

    elias new

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        // apiURL: `http://localhost:1337`,
+        apiURL: `https://secure-depths-28414.herokuapp.com/`,
         // apiURL: `https://creative-lab.netlify.app`,
         queryLimit: 1000,
         contentTypes: [
--- src/components/_App/layout.js
+++ src/components/_App/layout.js
@@ -2,11 +2,11 @@ import React from "react"
 import GoTop from "./GoTop"
 
 const Layout = ({ children }) => {
-    return (
-        <>
-            {children}
-            <GoTop scrollStepInPx="100" delayInMs="10.50" />
-        </>
-    )
+  return (
+    <>
+      {children}
+      {/* <GoTop scrollStepInPx="100" delayInMs="10.50" /> */}
+    </>
+  )
 }
 export default Layout
